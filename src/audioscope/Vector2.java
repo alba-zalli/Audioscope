9e82c6e9a06d91c75ef21ec46a9fbaf6f0f6cf8a
AmAbd4146 <AmAbd4146@7JG0C62>
2025-05-28 11:44:55 -0400

    4th Commit

--- src/audioscope/Vector2.java
+++ src/audioscope/Vector2.java
@@ -9,5 +9,36 @@ package audioscope;
  * @author AmAbd4146
  */
 public class Vector2 {
+        private float x;
+    private float y;
+    
+    public float getX(){
+        return x;
+    }
+    
+    public float getY(){
+        return y;
+    }
+    
+    public void setX(float num){
+        x = num;
+    }
+    
+    public void setY(float num){
+        y = num;
+    }
+    
+    public void addX(float num){
+        x = x + num;
+    }
+    
+    public void addY(float num){
+        y = y + num;
+    }
+    
+    public float magnitude(){
+        // Magnitude is pythagoreom theorem: c = square root of ( a^2  + b^2)
+        return (float) ( Math.sqrt( (Math.pow(x, 2) + Math.pow(y, 2)) ) );
+    }
     
 }
